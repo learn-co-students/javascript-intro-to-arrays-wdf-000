ef066205074b39c2ed9d021ea46f52a1fa6834a8
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-19 12:56:06 -0400

    done

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  array[1] = "Carter";
+  return array[1];
+}
+
+function removeElementFromArray(array){
+  array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(array){
+  var newNums = [];
+  array.forEach(function(number){
+    newNums.push(number + 5);
+  });
+  return newNums;
+}
