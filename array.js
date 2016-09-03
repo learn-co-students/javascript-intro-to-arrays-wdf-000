a9a90ad016007efcbb4d8a2b9d85e87b805c0ca0
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-03 10:36:19 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  return ['snickers', 'hundred grand','kitkat','skittles'];
+}
+
+function addElementToArray(num) {
+  num.push("A Christmas Story");
+  return num
+}
+
+function accessElementFromArray(arr) {
+  return arr[2];
+}
+
+function replaceElementInArray(arr) {
+  arr[1] = 'Carter';
+  return arr[1];
+}
+
+function removeElementFromArray(arr) {
+  arr.splice(2,2,"Roast Chicken");
+  return arr
+}
+
+function iterateArray(arr) {
+  var newNums = [];
+  arr.forEach(function(num) {
+    newNums.push(num+5);
+  });
+  return newNums;
+}
