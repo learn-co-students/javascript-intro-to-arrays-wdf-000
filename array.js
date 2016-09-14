79d0797e809f73ff93879438b183e5174d102b15
Derick Quartey <niikwartey1@gmail.com>
2016-09-14 14:21:21 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(movies) {
+  movies.push('A Christmas Story');
+  return movies;
+}
+
+function accessElementFromArray(arr) {
+  return arr[2]
+}
+
+function replaceElementInArray(arr) {
+  return arr[2] = 'Carter'
+}
+
+function removeElementFromArray(arr) {
+  arr.splice(2, 2, 'Roast Chicken')
+  return arr
+}
+
+function iterateArray(nums) {
+  return nums.map(function(num){ return num + 5 })
+}
