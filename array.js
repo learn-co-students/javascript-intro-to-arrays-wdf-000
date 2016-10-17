22f8141c2659191305700ff975307ecc2905d208
fidelscodes <fidelscodes@gmail.com>
2016-10-16 20:42:26 -0400

    Done

--- array.js
+++ array.js
@@ -1,1 +1,39 @@
 'use strict';
+function createArray() {
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+}
+
+function addElementToArray(array) {
+  array.push('A Christmas Story');
+  
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  array.splice(1, 1, 'Carter');
+
+  return array[1];
+}
+
+function removeElementFromArray(array) {
+  array.splice(2, 2, 'Roast Chicken');
+
+  return array;
+}
+
+function iterateArray(numbers) {
+  const newNums = [];
+
+  // arrow function breaks test although they pass in the browser... fun stuff lol
+  // numbers.forEach(num => newNums.push(num + 5));
+
+  numbers.forEach(function(num) {
+    newNums.push(num + 5);
+  });
+
+  return newNums;
+}
