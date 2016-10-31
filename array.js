3bb0156bc825d7db377397298b09b9ba463e90ab
Isuru <github email address>
2016-10-31 16:29:41 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  return [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ];
+}
+
+function addElementToArray(){
+  return [ 'Home Alone', 'Love Actually', 'Elf', 'A Christmas Story' ]
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  array[1] = 'Carter';
+  return array[1];
+}
+
+function removeElementFromArray(array){
+  var spliced  = array.splice(0,2)
+  spliced.push("Roast Chicken")
+  return spliced
+}
+
+
+function iterateArray(array){
+  var newNums = [];
+  array.forEach(function(number){
+    newNums.push(number + 5);
+  });
+  return newNums;
+}
