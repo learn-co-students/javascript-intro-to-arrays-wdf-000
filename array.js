38d722c0d6cd5da4f0721c2c9f91ff72a22b9c8e
github username <github email address>
2016-12-06 18:12:05 -0500

    Done

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+function createArray(){
+   var array = ["snickers","hundred grand","kitkat","skittles"];
+   return array;
+}
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  return array[2] = "Carter"
+}
+
+function removeElementFromArray(array){
+  array.splice(2, 2, 'Roast Chicken');
+
+  return array;
+}
+
+function iterateArray(array){
+  var newNums = [];
+  array.forEach(function(number) {
+    newNums.push(number+5)
+  });
+  return newNums;
+}
