192a6fc8ac5c4fcf63272a7d653a07e2b80c0608
Kelene Chung <simoy93@gmail.com>
2016-12-07 15:21:26 -0500

    DONE

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray(){
+	return ["snickers", "hundred grand", "kitkat", "skittles"]
+}
+
+function addElementToArray(array){
+	array.push("A Christmas Story")
+	return array
+}
+
+function accessElementFromArray(statesArr){
+	return statesArr[2]
+}
+
+function replaceElementInArray(nameArr){
+	return nameArr[1] = "Carter"
+}
+
+function removeElementFromArray(dishes){
+	dishes.splice(2, 3, 'Roast Chicken')
+	return dishes
+}
+
+function iterateArray(numbers){
+	var newNums = [];
+	numbers.forEach(function(num){
+		newNums.push(num + 5);	
+	});
+
+	return newNums
+}
